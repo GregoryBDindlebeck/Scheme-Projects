d29654e9915accafdac2d1fd9fecca847c0e35be
GregoryBDindlebeck <gregorybdindlebeck@gmail.com>
2017-06-03 11:51:13 -0700

    Update sec in scheme.scm

--- sec in scheme.scm
+++ sec in scheme.scm
@@ -1,4 +1,7 @@
-(define (secc z) (+ 1 
+;This is my first scheme program. 
+;I remember wanted to create a numerical approximation for secant. So I did
+;Thanks Wolfram! http://mathworld.wolfram.com/Secant.html
+(define (sec z) (+ 1 
 					(* 0.5 (* z z)) 
 					(* (/ 5 24) (* z z z z))
 					(* (/ 61 720) (* z z z z z z))
